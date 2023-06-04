dde564df9beaa891ba8b0e5ed8b18999e3059052
ItzNesbro Dev <asnesbeer3@gmail.com>
2023-06-04 04:40:57 +0000

    updated default lang to eng

--- src/components/chip.vue
+++ src/components/chip.vue
@@ -17,12 +17,12 @@
 </template>
 
 <script>
-import { i18n } from '../js/app';
+import { i18n } from "../js/app";
 
 export default {
   setup() {
     let selectedLang = i18n.global.locale;
-    const langs = ['ja', 'es', 'en'];
+    const langs = ["en", "ja", "es"];
 
     // Change language function
     const changeLanguage = (event) => {
